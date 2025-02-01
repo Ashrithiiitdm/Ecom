b21567b469e42bbb99db3b31712b6aeb2479f37c
Ashrithiiitdm <cs22b2025@iiitdm.ac.in>
2025-02-01 23:36:42 +0530

    Added user, Product models and controllers done

---
+++ backend/controllers/userController.js
@@ -0,0 +1,111 @@
+import { User } from "../models/userModel.js";
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const regToken = (user_id) => {
+    return jwt.sign({ user_id }, process.env.JWT_SECRET);
+};
+
+export const loginUser = async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+
+        //Check if user exists or not..
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+
+        //Check if password is correct
+        const validPass = await bcrypt.compare(password, user.password);
+
+        if (!validPass) {
+            return res.json({
+                success: false,
+                message: 'Invalid password'
+            });
+        }
+
+        //Sign a token
+        const token = regToken(user._id);
+        return res.json({
+            success: true,
+            message: 'User logged in successfully',
+            token
+        });
+
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+};
+
+export const regUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        //Check if user already exists
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.json({
+                success: false,
+                message: 'User already exists'
+            });
+        }
+
+        if (!validator.isEmail(email)) {
+            return res.json({
+                success: false,
+                message: 'Please enter a valid email'
+            });
+        }
+
+        if (password.length < 8) {
+            return res.json({
+                success: false,
+                message: 'Please enter a strong password'
+            });
+        }
+
+        //Hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        //Save the user
+        const user = newUser.save();
+
+        //Sign a token
+        const token = regToken(user._id);
+
+        return res.json({
+            success: true,
+            message: 'User registered successfully',
+            token
+        })
+
+    }
+    catch (err) {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'An error occurred'
+        });
+    }
+};
+
+export const adminLogin = async (req, res) => {
+};
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './db.js';
+import { connectCloudinary } from './cloudinary.js';
+import userRouter from './routes/user.js';
+import productRouter from './routes/products.js';
 
 dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 5000;
+connectDB();
+connectCloudinary();
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
 
 //Api endpoints
+
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 
